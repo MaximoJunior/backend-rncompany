b859d8efe4fcdfea43ea78fb4c5f1e9fee9f8d31
MaximoJunior <studentdeveloper19@gmail.com>
2022-01-30 09:19:29 -0500

    example.env added

--- database/config.js
+++ database/config.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
 
     try {
+        // Change MONGODB_CNN to DEV_MONGODB_CNN for using a local DB
         await mongoose.connect( process.env.MONGODB_CNN, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
